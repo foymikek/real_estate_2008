62514b67a1aca851db282322bd60af7bf5f567b9
Michael Foy <foymikek@gmail.com>
2020-08-26 09:41:51 -0600

    Add passing tests for room attribute

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,21 @@
 class House
-  attr_reader :price, :address
-  def initialize
+  attr_reader :price, :address, :rooms
+  def initialize(price, address)
     @price   = price
     @address = address
-    # @rooms   = Array.new
+    @rooms   = Array.new
   end
+
+  def price
+    @price.gsub(/\D/,'').to_i
+  end
+
+  def add_room(room)
+    @rooms.push(room)
+  end
+
+  def method_name
+
+  end
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -8,4 +8,24 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
   end
+
+  def test_house_rooms_atrribute
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal 400000, house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms
+  end
+
+  def test_add_rooms_to_rooms_attribute
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 10, '15')
+    house = House.new("$400000", "123 sugar lane")
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
+  end
+
 end
